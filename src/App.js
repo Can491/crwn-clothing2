1c70e2c46c75e959bf8338823d76672330dc5c55
unknown <cancheng1993@gmail.com>
2021-02-20 01:29:42 +0800

    I have finished collection-item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop-page/shop-page.component'
+import React from 'react';
 import './App.css';
+import { Route } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path='/' component = { HomePage }/>
+      <Route exact path='/shop' component = { ShopPage }/>
     </div>
   );
 }
---
+++ src/page/shop-page/shop-page.component.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionReview from '../../component/collection-review/collection-review.component';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections: SHOP_DATA
+        }
+
+    }
+
+    render() {
+        const {collections} = this.state;
+        return(
+            <div className = "shoppage">
+                {
+                    collections.map(({id,...otherCollectionProps}) => (
+                        <CollectionReview key= {id} {...otherCollectionProps}/>)
+                    )}
+                </div>
+        );
+    }
+}
+
+export default ShopPage;
